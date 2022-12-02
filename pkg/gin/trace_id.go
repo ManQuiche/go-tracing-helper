89549f2c716560dc5ccbaa17d1ffca174d1e2bf1
rabotin <mateo.rabotin@dejamobile.com>
2022-12-02 13:16:35 +0100

    feat: traceID gin middleware

---
+++ pkg/gin/trace_id.go
@@ -0,0 +1,18 @@
+package gin
+
+import (
+	"github.com/gin-gonic/gin"
+	"gitlab.com/ricardo-public/tracing/pkg/tracing"
+	"go.opentelemetry.io/otel/trace"
+	"log"
+)
+
+func LogTraceID(gtx *gin.Context) {
+	gtx.Next()
+	a := gtx.Request.Context().Value(tracing.HttpSpanKey)
+
+	span, ok := a.(trace.Span)
+	if ok {
+		log.Printf("traceID: %d", span.SpanContext().TraceID())
+	}
+}
