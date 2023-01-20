9ce895810cfa8843dfbf8bcda4511fac58f6e482
rabotin <mateo.rabotin@dejamobile.com>
2023-01-20 13:27:48 +0100

    feat(gin): added TraceRequest func

--- pkg/gin/trace_id.go
+++ pkg/gin/trace_id.go
@@ -1,8 +1,11 @@
 package gin
 
 import (
+	"context"
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"gitlab.com/ricardo-public/tracing/pkg/tracing"
+	semconv "go.opentelemetry.io/otel/semconv/v1.12.0"
 	"go.opentelemetry.io/otel/trace"
 	"log"
 )
@@ -16,4 +19,14 @@ func LogTraceID(gtx *gin.Context) {
 			log.Printf("path: %s | traceID: %s", gtx.Request.URL, span.SpanContext().TraceID().String())
 		}
 	}
+}
+
+func TraceRequest(gtx *gin.Context) {
+	ctx, span := tracing.Tracer.Start(gtx.Request.Context(), fmt.Sprintf("%s %s", gtx.Request.Method, gtx.FullPath()))
+	span.SetAttributes(semconv.HTTPURLKey.String(gtx.Request.URL.String()))
+	gtx.Request = gtx.Request.WithContext(context.WithValue(ctx, tracing.HttpSpanKey, span))
+
+	gtx.Next()
+
+	LogTraceID(gtx)
 }
